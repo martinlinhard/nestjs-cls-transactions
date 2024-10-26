932c9854b05de842047ebd0eb894ba98d417c788
Martin Linhard <martin.linhard@dectria.com>
2024-10-26 13:46:40 +0200

    feat: init transaction handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { getDataSourceToken, TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './domain/user/user.module';
 import { AccountModule } from './domain/account/account.module';
+import { ClsModule } from 'nestjs-cls';
+import { TransactionalAdapterTypeOrm } from '@nestjs-cls/transactional-adapter-typeorm';
+import { ClsPluginTransactional } from '@nestjs-cls/transactional';
 
 @Module({
   imports: [
+    ClsModule.forRoot({
+      global: true,
+      middleware: { mount: true },
+      plugins: [
+        new ClsPluginTransactional({
+          imports: [
+            // module in which the database instance is provided
+            TypeOrmModule,
+          ],
+          adapter: new TransactionalAdapterTypeOrm({
+            // the injection token of the database instance
+            dataSourceToken: getDataSourceToken(),
+          }),
+        }),
+      ],
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
