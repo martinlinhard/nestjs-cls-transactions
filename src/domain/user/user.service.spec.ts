239295dc6bdc5e69f8eb7afa1e4cf071ac95311b
Martin Linhard <martin.linhard@dectria.com>
2024-10-26 15:32:12 +0200

    feat: add example for safety measure

--- src/domain/user/user.service.spec.ts
+++ src/domain/user/user.service.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { AppModule } from '../../app.module';
+import { ClsService } from 'nestjs-cls';
 
 describe('UserService', () => {
   let service: UserService;
+  let clsService: ClsService;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,11 +13,16 @@ describe('UserService', () => {
     }).compile();
 
     service = module.get<UserService>(UserService);
+    clsService = module.get<ClsService>(ClsService);
   });
 
   test('Whether context works', async () => {
-    await expect(() => service.createOrUpdate()).rejects.toThrow();
-    const result = await service.userRepository.count();
-    expect(result).toEqual(0);
+    console.log("is active (outer)", clsService.isActive());
+    await clsService.runWith({}, async () => {
+      console.log("is active (inner)", clsService.isActive());
+      await expect(() => service.createOrUpdate()).rejects.toThrow();
+      const result = await service.userRepository.count();
+      expect(result).toEqual(0);
+    });
   });
 });
